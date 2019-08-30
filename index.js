3ba55446107b2dfe0c3fc767a85af5e8a691dc69
jaikumar-sm <jai.kumar@splashmath.com>
2019-08-30 13:24:24 +0530

    Added Comment from B2

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
     let d = 8;
     let e = 9;
     let f = 11;
-<<<<<<< HEAD
     //Added Comment in MASTER
-=======
     //Added from B1 Branch
     //New in B1
->>>>>>> B1
+    //Added in B2
 })()
